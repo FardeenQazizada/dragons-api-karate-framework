458b2dd9d12f067c7938e4fafb9ffb9c934970be
Fardeen Qazizada <ahmad_fardeen@hotmail.com>
2023-05-22 19:48:36 -0400

    TestRunner included

---
+++ src/test/java/api/runners/SmokeRunner.java
@@ -0,0 +1,14 @@
+package api.runners;
+
+import com.intuit.karate.junit5.Karate;
+
+public class SmokeRunner {
+	@Karate.Test
+
+	public Karate runTests() {
+		//run() method required path to feature files
+		//You can use the tags() method to specify the tags
+		return Karate.run("classpath:features").tags("Smoke");
+	}
+
+}
